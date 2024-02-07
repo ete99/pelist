376e804f9958ba83cb62b54143e1e11920b1f787
Stefan Vallet <stefanvallet99@gmail.com>
2024-02-07 17:22:32 -0500

    chore: add yarn.lock; add react-hot-toast

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Toaster />
     <BrowserRouter>
       <App />
     </BrowserRouter>
