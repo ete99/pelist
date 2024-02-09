3bce3e0d7f3acb8d42d4efb665c02a87870a65b1
Stefan Vallet <stefanvallet99@gmail.com>
2024-02-09 08:47:15 -0500

    feat: mas lindo

--- src/components/MovieDetail.tsx
+++ src/components/MovieDetail.tsx
@@ -27,47 +27,62 @@ const MovieDetail: React.FC = () => {
   }, [id]);
 
   return (
-    <div className="max-w-md mx-auto mt-8 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div className="max-w-md mx-auto mt-8 bg-white border rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <ImageLikeSkeleton
         className={"max-h-96 w-full object-contain"}
         src={movie.Poster}
         alt={movie.Title}
       />
-      <div className="p-6">
+      <div className="px-4 pt-2">
         <div className="inline">
           <h2 className="text-2xl font-bold mb-2 inline">
             {movie.Title || <Skeleton />}
           </h2>
-          <p className="text-sm text-gray-600 mb-2 inline ml-2">
-            {movie.Year || <Skeleton />}
-          </p>
         </div>
+
         <div className="flex flex-row items-start  text-sm text-gray-600 mb-2 align-middle">
-          <div className="flex align-middle">
-            <FaStar className="h-5" />
+          <div>
+            <p className="text-sm text-gray-600">
+              {movie.Type || <Skeleton />}
+            </p>
           </div>
-          <div className="ml-1 w-16 flex flex-row">
+          <p className="mx-1">{"•"}</p>
+          <div>
+            <p className="text-sm text-gray-600 mb-2 inline">
+              {movie.Year || <Skeleton />}
+            </p>
+          </div>
+          <p className="mx-1">{"•"}</p>
+          <div className="flex align-middle">
+            <FaStar className="h-5 text-yellow-500" />
+          </div>
+          <div className="mx-1 flex flex-row">
             {movie.imdbRating || (
               <div className="w-5">
                 <Skeleton />
               </div>
             )}
-            <p className="ml-1">/ 10</p>
+            <p className="ml-0.5"> /10</p>
           </div>
         </div>
         <p className="text-base text-gray-800 mb-4">
           {movie.Plot || <Skeleton />}
         </p>
-        <p className="text-sm text-gray-700 mb-2">
-          <span className="font-semibold">Director:</span>{" "}
-          {movie.Director || <Skeleton />}
-        </p>
-        <ul className="text-sm text-gray-700 mb-4">
-          {(movie.Actors &&
-            movie.Actors.split(",").map((actor: string) => (
-              <li key={actor}>{actor.trim()}</li>
-            ))) || <Skeleton count={3} />}
-        </ul>
+        <div className="flex space-x-4">
+          <p className="text-sm text-gray-700 mb-2">
+            <span className="font-semibold">Director:</span>{" "}
+            {movie.Director || <Skeleton />}
+          </p>
+          <div className="flex text-sm text-gray-700 mb-2">
+            <span className="font-semibold">Reparto:</span>{" "}
+            <ul className="ml-1 text-sm text-gray-700 mb-4 min-w-28">
+              {(movie.Actors &&
+                movie.Actors.split(",").map((actor: string) => (
+                  <li key={actor}>{actor.trim()}</li>
+                ))) || <Skeleton count={3} />}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
